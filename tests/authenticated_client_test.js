51d00af7594f8ea5898c973cdd628f951e22eb76
Craig Hammell <cilphex@gmail.com>
2016-01-15 12:15:08 -0800

    authenticated book request and tests

--- tests/authenticated_client_test.js
+++ tests/authenticated_client_test.js
@@ -1,12 +1,36 @@
 var assert = require('assert');
+var nock = require('nock');
 
 var CoinbaseExchange = require('../index.js');
+var authClient = new CoinbaseExchange.AuthenticatedClient();
 
 test('should require ordeId for cancelOrder', function(done) {
-  var authClient = new CoinbaseExchange.AuthenticatedClient();
-
   authClient.cancelOrder(null, function(err, resp, data) {
     assert(err);
     done();
   });
+});
+
+test('should return the order book', function(done) {
+  nock('https://api.exchange.coinbase.com')
+  .get('/products/BTC-USD/book?level=3')
+  .reply(200, {
+    'sequence': '3',
+    'bids': [
+      ['295.96', '0.05088265', '3b0f1225-7f84-490b-a29f-0faef9de823a']
+    ],
+    'asks': [
+      ['295.97', '5.72036512', 'da863862-25f4-4868-ac41-005d11ab0a5f']
+    ]
+  });
+
+  authClient.getProductOrderBook({level: 3}, function(err, resp, data) {
+    assert.ifError(err);
+
+    assert.equal(data.sequence, '3');
+    assert.deepEqual(data.bids, [['295.96', '0.05088265', '3b0f1225-7f84-490b-a29f-0faef9de823a']]);
+    assert.deepEqual(data.asks, [['295.97', '5.72036512', 'da863862-25f4-4868-ac41-005d11ab0a5f']]);
+
+    done();
+  })
 });
--- tests/public_client.js
+++ tests/public_client.js
@@ -5,7 +5,6 @@ var CoinbaseExchange = require('../index.js');
 var publicClient = new CoinbaseExchange.PublicClient();
 
 test('public client should return values', function(done) {
-  
   nock('https://api.exchange.coinbase.com')
   .get('/products/BTC-USD/ticker')
   .reply(200, {
@@ -20,7 +19,31 @@ test('public client should return values', function(done) {
     assert.equal(data.trade_id, 'test-id');
     assert(data.price, '9.00');
     assert(data.size, '5');
-    
+
     done();
   });
+});
+
+test('should return the order book', function(done) {
+  nock('https://api.exchange.coinbase.com')
+  .get('/products/BTC-USD/book?level=3')
+  .reply(200, {
+    'sequence': '3',
+    'bids': [
+      ['295.96', '0.05088265', '3b0f1225-7f84-490b-a29f-0faef9de823a']
+    ],
+    'asks': [
+      ['295.97', '5.72036512', 'da863862-25f4-4868-ac41-005d11ab0a5f']
+    ]
+  });
+
+  publicClient.getProductOrderBook({level: 3}, function(err, resp, data) {
+    assert.ifError(err);
+
+    assert.equal(data.sequence, '3');
+    assert.deepEqual(data.bids, [['295.96', '0.05088265', '3b0f1225-7f84-490b-a29f-0faef9de823a']]);
+    assert.deepEqual(data.asks, [['295.97', '5.72036512', 'da863862-25f4-4868-ac41-005d11ab0a5f']]);
+
+    done();
+  })
 });
